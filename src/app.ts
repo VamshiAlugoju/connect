5dd1a904eb58cc5574fe6cd6c2ac8faf5826222b
vamshi_ext <vamshi.alugoju@extwebtech.in>
2024-05-22 19:07:37 +0530

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
-app.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.on("play", (data) => {
+    console.log(data);
+    socket.emit("play");
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json("helos");
+});
+
+server.listen(3000, () => {
   console.log("listening on some port");
 });
